294810c73c38872a026b08001e3c007b48d9487a
Gerli Rand <gerli.rand@student.careeria.fi>
2023-11-18 16:22:16 +0200

    Esimerkki colorama moodulin väreistä

---
+++ PV8/colors.py
@@ -0,0 +1,8 @@
+from colorama import Fore, Back, Style
+
+print(Fore.BLUE + "Eri väristä tekstiä!")
+print(Back.LIGHTCYAN_EX + "Eri taustaväri :)")
+
+# palautettaan kaikki normaaliksi
+print(Style.RESET_ALL)
+print("Nyt ollaan takaisin normaalissa tilassa!")
