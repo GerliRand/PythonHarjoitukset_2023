6f49e30ec3a1199fc8bf8fe9d45908397dab6fc8
Gerli Rand <gerli.rand@student.careeria.fi>
2023-11-20 10:59:17 +0200

    Lisätty ympyrä, muutettu asetusta ja kokoja

--- PV8/pillowesim.py
+++ PV8/pillowesim.py
@@ -2,14 +2,18 @@
 from PIL import Image, ImageDraw
 
 # luodaan uusi kuva, koko: 100 x 30
-img = Image.new('RGB', (100, 40), color=(73, 109, 137))
+# värit - rgb
+img = Image.new('RGB', (500, 300), color=(73, 109, 137))
 
 # alustetaan piirto-objekti
 d = ImageDraw.Draw(img)
 
 # piirretään tekstiä
 # kordinaadit alkaa aina vasemmasta yläkulmasta
-d.text((10, 10), "Hello World", fill=(255, 255, 0))
+d.text((120, 80), "Hello World", fill=(255, 255, 0))
+
+# xy - aloituspiste on kohdassa 100, 100 - oikea alanurkka on kohdassa 200, 200
+d.ellipse((100, 100, 200, 200), fill=(192, 102, 217), outline=(0, 0, 0))
 
 # tallennetaan lopputulos tiedostoon (eli luo uuden tiedostoon)
 img.save('pil_text.png')
