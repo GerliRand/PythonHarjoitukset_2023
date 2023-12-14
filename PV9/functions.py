584b5c5c3aafd2584eff6ee374ce05bb3f3947f8
Gerli Rand <gerli.rand@student.careeria.fi>
2023-12-14 19:37:08 +0200

    Uudet funktiot(return vuosi ja numeron vertailu)

--- PV9/functions.py
+++ PV9/functions.py
@@ -12,3 +12,14 @@ def combine_text(first, last, age):
     print(f"Tervetuloa: {first} {last}!")
     print(f"Ikäsi on: {age} vuotta.")
 
+# apufunktio, joka palauttaa kuluvan vuoden numerona (voi käyttää datetime)
+def get_year():
+    year = 2023
+    return year
+
+# funktio joka päättelee annetusta numerosta onko kyseessä parillinen vai pariton luku
+def get_even_number_text(number):
+    if number % 2 == 0:
+        return "Parillinen"
+    else:
+        return "Pariton"
--- PV9/funktio_def_esim2.py
+++ PV9/funktio_def_esim2.py
@@ -2,3 +2,14 @@ from functions import *
 
 # kutsutaan funktiota ja annettaan tiedot
 combine_text("Herra", "Hakkarainen", 42)
+
+# get_year palauttaa tietoa return-komenolla
+# tarvitaan samalla muuttuja joka ottaa tiedosta kopion
+result = get_year()
+print(result)
+
+# kutsutaan apufunktiota joka päättele onko nro pariton tai parillinen
+value = input("Syötä jonkin numero:\n")
+value = int(value)
+result = get_even_number_text(value)
+print(result)
