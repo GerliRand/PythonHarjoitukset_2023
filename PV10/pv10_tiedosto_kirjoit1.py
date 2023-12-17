bb955094ba4e91677e27e7a57f239dbbcafd72b1
Gerli Rand <gerli.rand@student.careeria.fi>
2023-12-17 16:46:25 +0200

    Muutettiin hieman koodi w sijaan a, eli ei ylikirjoitetta enää tiedostoa

--- PV10/pv10_tiedosto_kirjoit1.py
+++ PV10/pv10_tiedosto_kirjoit1.py
@@ -1,10 +1,11 @@
 # aukaistaan yhteys tiedostoon -> kirjoitusmoodi (w)
 # luo lennosta uuden tiedoston
 # encodingin avulla saa ääkköset kuntoon!!!
-file_handle = open("muistio.txt", "w", encoding="utf-8")
+# jos käyttää w sijaan a - niin se ei kirjoittaa yli vaan lisää
+file_handle = open("muistio.txt", "a", encoding="utf-8")
 
 # kysytään käyttäjältä tallentava teksti
 text = input("Anna jotain tekstiä:\n")
 
 # kirjoitetaan tiedostoon käyttäjän antama teksti
-file_handle.write(text)
+file_handle.write(text + "\n")
